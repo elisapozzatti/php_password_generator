28f4bbbd60d3db6226cc42b739d94b490e91aa95
DanLevy86 <daniel.levy86@gmail.com>
2025-02-28 15:43:33 +0100

    Seconda milestone

---
+++ password.php
@@ -0,0 +1,27 @@
+<?php
+
+function getRandomString($n) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+
+    for ($i = 0; $i < $n; $i++) {
+        $index = random_int(0, strlen($characters) - 1);
+        $randomString .= $characters[$index];
+    }
+
+    return $randomString;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Ciao, <?php echo $_POST['username']?></h2>
+    <p>La tua password sicura è: <?php echo getRandomString($_POST['password']) ?></p>
+</body>
+</html>
